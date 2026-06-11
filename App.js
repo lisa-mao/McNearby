647f928ed8024fefa753b2126fff774b4dfb536c
lisa-mao <lisamao2007@gmail.com>
2026-06-11 21:26:37 +0200

    added map to app

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location'
 
 export default function App() {
+    const [location, setLocation] = useState(null)
+    const [errorMsg, setErrorMsg] = useState(null)
+    const mapRef = useRef(null)
+
+    useEffect(() => {
+        (async () => {
+            let {status} =await Location.requestForegroundPermissionsAsync()
+            if (status !== "granted") {
+                setErrorMsg("Permission to access location was denied")
+                return
+            }
+
+            let currentLocation = await Location.getCurrentPositionAsync({})
+            const {latitude, longitude} = currentLocation.coords
+            setLocation(currentLocation)
+
+            mapRef.current?.animateToRegion({
+                latitude,
+                longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+            }, 1000)
+        })()
+    }, [])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <MapView
+            style={styles.map}
+            showsUserLocation={true}
+            followsUserLocation={true}
+            initialRegion={{
+                latitude: 51.9225,
+                longitude: 4.47917,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }}
+        >
+            {location && (
+                <Marker coordinate={{
+                    latitude:location.latitude,
+                    longitude: location.longitude,
+
+                }}
+                        title="You are here"
+                        />
+            )}
+        </MapView>
+
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
